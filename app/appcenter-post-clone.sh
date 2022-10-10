13a41274f2aab9714d47d5ea40fc730268fdc21a
ankasani <ankasani@microsoft.com>
2022-10-10 20:45:59 +0530

    Add files via upload

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 set -ex
-export JAVA_OPTS='-XX:+IgnoreUnrecognizedVMOptions --add-modules java.se.ee'
-SDKMANAGER=$ANDROID_HOME/tools/bin/sdkmanager
+SDKMANAGER=$ANDROID_SDK_ROOT/cmdline-tools/latest/bin/sdkmanager
 echo y | $SDKMANAGER "ndk;21.4.7075529"
