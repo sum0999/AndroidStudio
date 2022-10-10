01d5f8529cdf17ba8a42e01dd0244299cb0505fd
ankasani <ankasani@microsoft.com>
2022-10-10 20:35:08 +0530

    Delete appcenter-post-clone.sh

--- appcenter-post-clone.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-export JAVA_OPTS='-XX:+IgnoreUnrecognizedVMOptions --add-modules java.se.ee'
-export JAVA_OPTS='-XX:+IgnoreUnrecognizedVMOptions --add-modules java.xml.bind'
-SDKMANAGER=$ANDROID_HOME/tools/bin/sdkmanager
-echo y | $SDKMANAGER "ndk;21.4.7075529"
